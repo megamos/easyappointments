a4baa7815fe1f5f4a9fdde73eb18140e97869eae
Marcus Mueller <mm.swedev@gmail.com>
2022-04-18 13:30:42 +0200

    fix/Test settings

---
+++ tests/phpunit/engine/Api/V1/CreateAppointmentTest.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * CLG: Create booking with several rooms, guests, and relatives
+*/
+
+namespace EA\Engine\Api\V1;
+
+use PHPUnit\Framework\TestCase;
+use EA\
+
+class CreateAppointmentTest extends TestCase
+{
+  public function testCreatingAppointment()
+  {
+    //$type = new Url('http://localhost/easyappointments/index.php');
+    $result = '';//$type->get();
+    $this->assertIsString($result);
+  }
+}
