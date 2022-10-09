6ed5784220b53c80a3764b5607d40ec4c50a3555
Marcus Mueller <mm.swedev@gmail.com>
2022-10-09 18:58:10 +0200

    Started on CLG validations

--- application/sql/insert_extra_services.sql
+++ application/sql/insert_extra_services.sql
@@ -1,8 +1,11 @@
 INSERT INTO `ea_services`(`name`, `color`, `duration`, `price`, `currency`, `description`, `availabilities_type`, `attendants_number`, `is_all_rooms`) 
 VALUES 
+('50-års födelsedag','#F5BE00','1440','0','','Boka hela gården för 50-års kallas','flexible','20','1'),
+('75-års födelsedag','#F5BE00','1440','0','','Boka hela gården för 75-års kallas','flexible','20','1'),
 ('Brölopp','#F5BE00','1440','0','','Boka hela gården för ett brölopp','flexible','20','1'),
 ('Dop','#F5BE00','1440','0','','Boka hela gården för ett dop','flexible','20','1'),
 ('Jul','#F5BE00','1440','0','','Boka hela gården för att fira jul','flexible','20','1'),
-('Nyår','#F5BE00','1440','0','','Boka hela gården för Nyår','flexible','20','1')
+('Nyår','#F5BE00','1440','0','','Boka hela gården för Nyår','flexible','20','1'),
+('Släktguide','#F5BE00','1440','0','','Boka hela gården för Nyår','flexible','20','0')
 
 -- Färger bör kollas över, om de borde va samma, samt vilken färg som passar bäst
